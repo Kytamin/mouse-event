59071ce24ffc4e5846496f10340d6b10d84aeca5
KY <nguyenhoangky23121997@gmail.com>
2023-03-13 08:03:51 +0700

    1

---
+++ mouse.js
@@ -0,0 +1,14 @@
+
+window.onload = init;
+function init() {
+    imgObj = document.getElementById('myImage');
+    imgObj.style.position = 'relative';
+    imgObj.style.left = '0px';
+    }
+function moveRight() {
+    imgObj.style.left = parseInt(imgObj.style.left)+10  + 'px';
+}
+function moveLeft(){
+    imgObj.style.left=parseInt(imgObj.style.left)-10+'px';
+}
+
